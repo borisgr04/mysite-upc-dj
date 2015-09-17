8109656d08959ce05ff465685c0b51cd0d0c6891
AnyaMiyeth <anyamiyeth@gmail.com>
2015-09-16 22:04:50 -0500

    Cambio de vista index

--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from .models import Question
+
 
 """ Primera version
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 """
+"""
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([p.question_text for p in latest_question_list])
     return HttpResponse(output)
+"""
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
